9519c9065158d737ba854dbd07f60122675b8508
Everton Mendonça <evertonmj@gmail.com>
2016-07-21 10:30:33 -0300

    execution time reduction

--- war.cpp
+++ war.cpp
@@ -1,5 +1,4 @@
 //war.cpp
-//#include <iostream>
 #include <stdio.h>
 #include <vector>
 #include <numeric>
@@ -8,35 +7,24 @@
 
 using namespace std;
 
-int sum(vector<int>::iterator begin, vector<int>::iterator end) {
-  int total = 0;
-  for(vector<int>::iterator it = begin; it != end; it++) {
-    total += *it;
-  }
-
-  return total;
-}
-
 int main() {
-  int n_sections, section, cut_pos = 1, current_val, previous_val = 0;
+  int n_sections, section, cut_pos = 1, current_val, previous_val = 0, total_sections = 0;
   vector<int> sections;
 
-  //cin >> n_sections;
   scanf("%d\n", &n_sections);
 
   if(n_sections <= MAX_SECTIONS) {
     for(int i = 0; i < n_sections; i++) {
-      //cin >> section;
       scanf("%d", &section);
       if(section <= MAX_SECTION_SIZE) {
         sections.push_back(section);
+        total_sections += section;
       }
     }
 
     for(vector<int>::iterator it = sections.begin(); it != sections.end();it++) {
       previous_val += *it;
-      if(previous_val == sum(it + 1, sections.end())) {
-        //cout << cut_pos;
+      if(previous_val == (total_sections - previous_val)) {
         printf("%d", cut_pos);
         break;
       }
