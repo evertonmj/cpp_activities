0a925bd013f7e84df9cb6244787f2fa6d8323e41
Everton Mendonca <evertonmj@gmail.com>
2016-10-15 13:00:19 -0300

    new problem

---
+++ quermesse.cpp
@@ -0,0 +1,32 @@
+//quermesse.cpp
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main() {
+	int n, test = 1, index = 1;
+
+	while(cin >> n && n != 0) {
+		int current;
+		list<int> line;
+
+		for(int i = 0; i < n; i++) {
+			cin >> current;
+
+			line.push_back(current);
+		}
+
+		for(list<int>::iterator it = line.begin(); it != line.end(); it++) {
+			if(index == *it) {
+				cout << "Teste " << test << endl << *it << endl;
+				break;
+			}
+			index++;
+		}
+
+		index = 1;
+		test++;
+	}
+	return 0;
+}
